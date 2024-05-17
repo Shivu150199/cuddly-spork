23071bafb05fcc1411bf5fc5d331b29a29d405f3
Shivu150199 <shivamsinghgautam397@gmail.com>
2024-05-17 11:35:20 +0530

    deploy

--- api/index.js
+++ api/index.js
@@ -1,9 +1,16 @@
 import express from 'express'
+import path from 'path'
 
+const __dirname = path.resolve()
 const app = express()
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+app.use(express.static(path.join(__dirname, '/client/dist')))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
 })
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
